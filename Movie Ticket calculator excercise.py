496764030918e877fbde2528a3ea9890dfea4e12
svnszns <108220450+svnszns@users.noreply.github.com>
2026-03-15 22:58:52 +0200

    calculate the final price

--- Movie Ticket calculator excercise.py
+++ Movie Ticket calculator excercise.py
@@ -22,6 +22,31 @@ else:
     print('User does not qualify for membership discount')
 print('Discount:', discount)
 
+base_price = 15
+age = 21
+seat_type = 'Gold'
+show_time = 'Evening'
+final_price = 0;
+
+if age > 17:
+    print('User is eligible to book a ticket')
+
+if age >= 21:
+    print('User is eligible for Evening shows')
+else:
+    print('User is not eligible for Evening shows')
+
+is_member = False
+is_weekend = False
+
+discount = 0
+if is_member and age >= 21:
+    discount = 3
+    print('User qualifies for membership discount')
+else:
+    print('User does not qualify for membership discount')
+print('Discount:', discount)
+
 extra_charges = 0
 if is_weekend or show_time == 'Evening':
     extra_charges = 2
@@ -34,16 +59,19 @@ if age >= 21 or age >= 18 and (show_time != 'Evening' or is_member):
     print('Ticket booking condition satisfied')
 
     service_charges = 0
-    
     if seat_type == 'Premium':
         service_charges = 5
-        print('Servce charges:', service_charges)
     elif seat_type == 'Gold':
         service_charges = 3
-        print('Servce charges:', service_charges)
     else:
         service_charges = 1
-        print('Servce charges:', service_charges)
+    print('Service charges:', service_charges)
 
+    
 else:
     print('Ticket booking failed due to restrictions')
+
+
+final_price = base_price + extra_charges + service_charges - discount
+
+final_price = print('Final price of ticket:', final_price)
