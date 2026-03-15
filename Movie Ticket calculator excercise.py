b0bbc2479f0ae171a37dabc46221c8ea1332cefd
svnszns <108220450+svnszns@users.noreply.github.com>
2026-03-15 21:28:18 +0200

    age checker and membership checker

--- Movie Ticket calculator excercise.py
+++ Movie Ticket calculator excercise.py
@@ -2,3 +2,20 @@ base_price = 15
 age = 21
 seat_type = 'Gold'
 show_time = 'Evening'
+is_member = True
+is_weekend = False
+discount = 0
+
+if age > 17:
+    print('User is eligible to book a ticket')
+
+
+if age >= 21:
+    print('User is eligible for Evening shows')
+else:
+    print('User is not eligible for Evening shows')
+
+
+if is_member == True:
+    discount = 3
+    print('User qualifies for membership discount')
