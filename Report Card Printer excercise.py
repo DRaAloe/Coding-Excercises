96ce5c54637bd7456ef21a10223200b5bce4a6ce
svnszns <108220450+svnszns@users.noreply.github.com>
2026-03-15 16:18:53 +0200

    Completion of excercise to print name , and chech data type.

---
+++ Report Card Printer excercise.py
@@ -0,0 +1,13 @@
+name = 'Alice'#string to store the name of the student
+print(name, type(name))#print the name and its type
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+
+print(score, type(score));
